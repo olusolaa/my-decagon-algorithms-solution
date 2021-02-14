6e123e9f5a56f68f99040b02084ac4e4e11ceb90
olusolaa <alaoolusolae@gmail.com>
2021-02-14 15:40:57 +0100

    changeClassName

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -1,6 +1,6 @@
 import java.util.NoSuchElementException;
 
-public class Test {
+public class LinkedList {
 
     Node first;
     Node last;
--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Test list = new Test();
+        LinkedList list = new LinkedList();
        // list.addFirst(20);
        // list.addLast(30);
         //list.addLast(40);
