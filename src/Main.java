d3c0fb795e5483106648988fff528b918aedf591
olusolaa <alaoolusolae@gmail.com>
2021-02-14 15:31:53 +0100

    printMiddle

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,25 @@
 import java.util.*;
 
 public class Main {
+
+
     public static void main(String[] args) {
-        LinkedList list = new LinkedList();
-        list.add(20);
-        list.add(40);
-        list.add(50);
-        System.out.println(list);
-        list.addFirst(30);
-        list.removeLast();
-        System.out.println(list);
-        var Array = 2;
-        System.out.println(Array);
+
+        Test list = new Test();
+       // list.addFirst(20);
+       // list.addLast(30);
+        //list.addLast(40);
+       // list.addLast(50);
+        //list.removeLast();
+       // list.removeFirst();
+        System.out.println(list.contains(20));
+        System.out.println(list.size());
+
+//        list.reverse();
+        int [] arr = list.toArray();
+        System.out.println(Arrays.toString(arr));
+  //      System.out.println(list.kthPointer(0));
+        list.printMiddle();
+
     }
 }
---
+++ src/Test.java
@@ -0,0 +1,153 @@
+import java.util.NoSuchElementException;
+
+public class Test {
+
+    Node first;
+    Node last;
+    int size;
+
+    private boolean isEmpty(){
+        return this.first == null;
+    }
+
+    private class Node {
+        private Node next;
+        private int value;
+
+        Node(int item) {
+            this.value = item;
+        }
+    }
+    public void addLast(int value){
+        Node node = new Node(value);
+        if (isEmpty())
+            first = last = node;
+        else {
+            last.next = node;
+            last =node;
+        }
+        size++;
+    }
+    public void addFirst(int value){
+        Node node = new Node(value);
+        if (isEmpty())
+            first = last = node;
+        else {
+            node.next=first;
+            first = node;
+        }
+        size++;
+    }
+
+    public int indexOf(int value){
+        var count = 0;
+        var current = first;
+        while (current != null){
+            if (current.value == value)
+                return count;
+            current = current.next;
+            count ++;
+        }
+        return -1;
+    }
+
+    public boolean contains(int value) {
+        return indexOf(value) != -1;
+    }
+
+    public void removeFirst() {
+        var second = first.next;
+        first.next = null;
+        first = second;
+        size --;
+    }
+
+//    public void removeLast(){
+//        var current = first;
+//        while (current != null){
+//            if (current.next == last) break;
+//            current = current.next;
+//
+//        }
+//        last = current;
+//        last.next = null;
+//    }
+
+    public void removeLast(){
+        if (isEmpty())
+            throw new NoSuchElementException();
+
+        var current = first;
+        if (first == last)
+            first = last = null;
+        else{
+            while (current.next.next!= null) {
+                current = current.next;
+            }
+            current.next = null;
+            last = current;
+        }
+        size--;
+    }
+
+    public int size(){
+        return size;
+    }
+
+    public int [] toArray(){
+        int [] arr = new int[size];
+        var current = first;
+        var count = 0;
+        while (current !=null){
+            arr[count] = current.value;
+            count++;
+            current = current.next;
+        }
+        return arr;
+    }
+
+    public void reverse(){
+        var prev = first;
+        var current = first.next;
+
+        while (current != null){
+            var next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        last = first;
+        last.next=null;
+
+    }
+
+    public int kthPointer(int k) {
+        if (isEmpty())
+            throw new NoSuchElementException();
+        var a = first;
+        var b = first;
+        for(int i =0; i< k-1; i++){
+            b =b.next;
+        }
+        while (b !=last){
+            a = a.next;
+            b = b.next;
+        }
+        return a.value;
+    }
+
+    public void printMiddle(){
+        if (isEmpty())
+            throw new IllegalStateException();
+        var a = first;
+        var b = first;
+        while (b != last && b.next != last){
+            b = b.next.next;
+            a = a.next;
+        }
+        if (b == last)
+            System.out.println( a.value);
+        else System.out.println(a.value+ " " + a.next.value);
+    }
+}
+
